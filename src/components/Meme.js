efe37916159d34fc1b6924518f4843dab08eb9c8
JaiveerS <jaiveer_@hotmail.com>
2022-01-27 19:23:44 -0500

    setup fetching random meme

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,15 +1,25 @@
 import React from "react";
+import memesData from "../memesData";
 
 
 function Meme(){
+    function HandleClick(){
+        const size = memesData.data.memes.length;
+        const index = Math.round(Math.random() *size)
+        const data = memesData.data.memes
+        return (
+            console.log(data[index].url)
+        )
+    }
+
     return(
         <main>
-            <form className="form">
+            <div className="form">
                     <input className="form--input" type="text" placeholder="Top text"/>
                     <input className="form--input" type="text" placeholder="Bottom text" />
                     {/* add a onclick for the submit button */}
-                    <input className="form--submit" type="submit" value="Get a new meme image 🖼"/> 
-            </form>
+                    <button className="form--submit" onClick={HandleClick}>Get a new meme image 🖼</button> 
+            </div>
         </main>
     )
 }
